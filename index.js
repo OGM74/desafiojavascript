343875f81b9210cf0622ea6b295627f087aa4629
OGM74 <olga.gonzalezm@hotmail.com>
2024-03-03 00:11:25 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+let precioInicial = 400000;
+let cantidadInicial = 1;
+
+let precio = document.querySelector(".precio");
+precio.innerHTML = precioInicial;
+let cantidad = document.querySelector(".cantidad");
+cantidad.innerHTML = cantidadInicial;
+let total = document.querySelector(".total");
+total.innerHTML = precioInicial;
+let nombre = prompt("Ingrese su nombre");
+alert("Desafio Javascript " + nombre);
+
